819f350115fa7c1c02892e0a48d0c2005d01ac32
Alex <git@pentive.com>
2023-05-30 21:21:35 -0500

    pass reactive prop to plugin

--- my-solid-plugin/src/Comp.tsx
+++ my-solid-plugin/src/Comp.tsx
@@ -1,3 +1,7 @@
-export default () => {
-  return <h1>Hello world!!!</h1>;
-};
+import { VoidComponent } from "solid-js"
+
+const Comp: VoidComponent<{ i: number }> = (props) => {
+  return <h1>Hello world!!! {props.i} x</h1>
+}
+
+export default Comp
--- my-solid-project/src/App.tsx
+++ my-solid-project/src/App.tsx
@@ -1,6 +1,12 @@
-import { createSignal, type Component, createResource } from "solid-js"
+import {
+  createSignal,
+  type Component,
+  createResource,
+  VoidComponent,
+} from "solid-js"
 import styles from "./App.module.css"
 import { Dynamic } from "solid-js/web"
+import { script } from "./script"
 
 // https://stackoverflow.com/a/57255653
 async function toModule<T>(script: string) {
@@ -14,30 +20,15 @@ async function toModule<T>(script: string) {
 const App: Component = () => {
   const [count, setCount] = createSignal(0)
   const [plugin] = createResource(async () => {
-    const script = `function template(html, check, isSVG) {
-  const t = document.createElement("template");
-  t.innerHTML = html;
-  let node = t.content.firstChild;
-  if (isSVG) node = node.firstChild;
-  return node;
-}
-
-const _tmpl$ = /*#__PURE__*/template(\`<h1>Hello world!!!</h1>\`);
-const Comp = (() => {
-  return _tmpl$.cloneNode(true);
-});
-
-export { Comp as default };
-`
     let encodedPlugin = await toModule<{
-      default: Component
+      default: VoidComponent<{ i: number }>
     }>(script)
     return encodedPlugin.default
   })
   return (
     <div class={styles.App}>
       <button onClick={() => setCount((x) => x + 1)}>Inc {count()}</button>
-      <Dynamic component={plugin()} />
+      <Dynamic component={plugin()} i={count()} />
     </div>
   )
 }
