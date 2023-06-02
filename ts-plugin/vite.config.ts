ebeb7f802bb872d9c24126217b6ea8f866741b00
Alex <git@pentive.com>
2023-06-02 15:55:28 -0500

    add vite.config

---
+++ ts-plugin/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  build: {
+    minify: false,
+    rollupOptions: {
+      external: [
+        "solid-js"
+      ]
+    },
+    target: "esnext",
+    lib: {
+      entry: "src/counter.ts",
+      fileName: "index",
+      formats: ["es"],
+    },
+  },
+});
